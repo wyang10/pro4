b97ed1983927ce127b48f92d7f61195982081989
Weihong Yang <97855340+wyang10@users.noreply.github.com>
2022-12-13 10:42:47 -0800

    Update connection.js

--- server/connection.js
+++ server/connection.js
@@ -1,6 +1,6 @@
 const {MongoClient} = require("mongodb");
 
-const uri = mongodb+srv://whyang:yang890522@cluster0.jn664ob.mongodb.net/?retryWrites=true&w=majority || "mongodb://localhost:27017/?maxPoolSize=200";
+const uri = process.env.MONGOURI || "mongodb://localhost:27017/?maxPoolSize=200";
 
 const client = new MongoClient(uri);
 client.connect().then(() => {
